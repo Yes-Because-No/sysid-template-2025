1c49f4bcf7d0e2305771f49d5887af5d4015dbb6
Yes-Because-No <alex.tichindelean@gmail.com>
2025-01-24 14:18:25 -0500

    Add constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import com.ctre.phoenix6.signals.InvertedValue;
+
+public class Constants {
+    public static final int MOTOR_1ID = 0;
+    public static final int MOTOR_2_ID = 1;
+    public static final double CURRENT_LIMIT = 0;
+    public static final double ROTATIONS_TO_LINEAR_UNITS = 0;
+    public static final InvertedValue MOTOR_1_INVERTED = InvertedValue.Clockwise_Positive;
+    public static final InvertedValue MOTOR_2_INVERTED = InvertedValue.Clockwise_Positive;
+}
