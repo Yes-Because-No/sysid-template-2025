f57fb98194dbd05af91be52a8abfc24fc3cae2a1
Yes-Because-No <alex.tichindelean@gmail.com>
2025-01-24 14:18:42 -0500

    Apply motor configurations before running sysid

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -11,18 +11,56 @@ import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import frc.robot.Constants;
+
 public class Elevator extends SubsystemBase {
-    private int MOTOR_1ID = 0;
-    private int MOTOR_2_ID = 1;
+
+    private TalonFX motor1 = new TalonFX(Constants.MOTOR_1ID);
+    private TalonFX motor2 = new TalonFX(Constants.MOTOR_2_ID);
+
+    private TalonFXConfigurator motor1Configurator = motor1.getConfigurator();
+    private TalonFXConfigurator motor2Configurator = motor2.getConfigurator();
+
+    private CurrentLimitsConfigs currConf = new CurrentLimitsConfigs();
+    private MotorOutputConfigs motor1OutputConfigs = new MotorOutputConfigs();
+    private MotorOutputConfigs motor2OutputConfigs = new MotorOutputConfigs();
     
-    private TalonFX motor1 = new TalonFX(MOTOR_1ID);
-    private TalonFX motor2 = new TalonFX(MOTOR_2_ID);
+    private FeedbackConfigs feedbackConfigs = new FeedbackConfigs();
 
     private StrictFollower motor1Follower = new StrictFollower(motor1.getDeviceID());
 
+    public Elevator(){
+
+            currConf.SupplyCurrentLimitEnable = true;
+            currConf.SupplyCurrentLimit = Constants.CURRENT_LIMIT;
+
+            motor1Configurator.apply(currConf);
+            motor2Configurator.apply(currConf);
+
+            motor1OutputConfigs.Inverted = Constants.MOTOR_1_INVERTED;
+            motor2OutputConfigs.Inverted = Constants.MOTOR_2_INVERTED;
+
+            
+
+            motor1Configurator.apply(motor1OutputConfigs);
+            motor2Configurator.apply(motor2OutputConfigs);
+
+            feedbackConfigs.SensorToMechanismRatio = Constants.ROTATIONS_TO_LINEAR_UNITS;
+
+            motor1Configurator.apply(feedbackConfigs);
+            motor2Configurator.apply(feedbackConfigs);
+
+            motor2.setControl(motor1Follower);
+        }
+
+    
     private final MutVoltage sysIdVoltage = Volts.mutable(0);
     private final MutDistance sysIdPosition =Meters.mutable(0);
     private final MutLinearVelocity sysIdVelocity = MetersPerSecond.mutable(0);
@@ -34,9 +72,7 @@ public class Elevator extends SubsystemBase {
             }, this));
 
         
-    public Elevator(){
-        motor2.setControl(motor1Follower);
-    }
+    
     private void setVoltage(double voltage) {
         motor1.setVoltage(voltage);
     }
