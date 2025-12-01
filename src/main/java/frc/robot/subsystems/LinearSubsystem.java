ffe097c0207a839d2b9a15f486d6ad64ceab708d
Yes-Because-No <alex.tichindelean@gmail.com>
2025-12-01 17:23:43 -0500

    Iterate through map object of motors to allow for variable number of motors to be added to sysid mechanism

--- src/main/java/frc/robot/subsystems/LinearSubsystem.java
+++ src/main/java/frc/robot/subsystems/LinearSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -8,14 +9,11 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import com.ctre.phoenix6.SignalLogger;
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.FeedbackConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.StrictFollower;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import frc.robot.Constants;
 
@@ -29,10 +27,23 @@ public class LinearSubsystem extends SubsystemBase {
             config.Feedback.SensorToMechanismRatio = Constants.ROTATIONS_TO_LINEAR_UNITS;
             config.CurrentLimits.StatorCurrentLimitEnable = true;
             config.CurrentLimits.StatorCurrentLimit = Constants.CURRENT_LIMIT;
+
+            for (Map.Entry<Integer, Boolean> entry : motorMap.entrySet()){
+                int CANID = entry.getKey().intValue();
+                boolean inversion = entry.getValue().booleanValue();
+                TalonFX motor = new TalonFX(CANID);
+                config.MotorOutput.Inverted = inversion? InvertedValue.Clockwise_Positive: InvertedValue.CounterClockwise_Positive;
+                motor.getConfigurator().apply(config);
+                motors.add(motor);
+            }
+
+            for (int i = 1; i < motors.size(); i++){
+                motors.get(0).setControl(new StrictFollower(motors.get(0).getDeviceID()));
+            }
         }
 
     public void setVoltage(double voltage){
-        
+        motors.get(0).setControl(new VoltageOut(MathUtil.clamp(voltage, -12, 12)));
     }
 
      private SysIdRoutine sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(
