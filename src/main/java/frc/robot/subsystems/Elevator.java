c8dd7b9a2f1f0e29b1a1402599aedbbe3aab40f5
Yes-Because-No <alex.tichindelean@gmail.com>
2025-12-01 15:50:14 -0500

    Change SysID to use Phoenix 6 Signal Logger

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,21 +1,17 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.units.measure.MutDistance;
-import edu.wpi.first.units.measure.MutLinearVelocity;
-import edu.wpi.first.units.measure.MutVoltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Volts;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.StrictFollower;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import frc.robot.Constants;
@@ -60,40 +56,27 @@ public class Elevator extends SubsystemBase {
             motor2.setControl(motor1Follower);
         }
 
-    
-    private final MutVoltage sysIdVoltage = Volts.mutable(0);
-    private final MutDistance sysIdPosition =Meters.mutable(0);
-    private final MutLinearVelocity sysIdVelocity = MetersPerSecond.mutable(0);
-    private SysIdRoutine sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(),
-            new SysIdRoutine.Mechanism(voltage -> setVoltage(voltage.magnitude()), log -> {
-                log.motor("Elevator").voltage(sysIdVoltage.mut_replace(getVoltage(), Volts))
-                        .linearPosition(sysIdPosition.mut_replace(getPosition(), Meters))
-                        .linearVelocity(sysIdVelocity.mut_replace(getVelocity(), MetersPerSecond));
-            }, this));
-
-        
-    
-    private void setVoltage(double voltage) {
-        motor1.setVoltage(voltage);
+    public void setVoltage(double voltage){
+        motor1.setControl(new VoltageOut(voltage));
     }
 
-    private double getVoltage() {
-        return motor1.getMotorVoltage().getValueAsDouble();
-    }
-
-    private double getPosition() {
-        return motor1.getPosition().getValueAsDouble();
-    }
-
-    private double getVelocity() {
-        return motor1.getVelocity().getValueAsDouble();
-    }
+     private SysIdRoutine sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(
+            null,
+            null,
+            null,
+            (state)-> SignalLogger.writeString("state", state.toString())
+        ),
+        new SysIdRoutine.Mechanism(
+            voltage -> setVoltage(voltage.magnitude()), 
+            null, 
+            this
+    ));
 
     public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
         return sysIdRoutine.quasistatic(direction);
-    }
-
-    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+     }
+     
+     public Command sysIdDynamic(SysIdRoutine.Direction direction) {
         return sysIdRoutine.dynamic(direction);
-    }
+     }
 }
